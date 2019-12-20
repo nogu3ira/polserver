08d1d04120427c1ddeef9fde7aa345b5dba6cbef
Adriano Nogueira <nogueirajp@gmail.com>
2019-12-20 13:18:07 -0300

    Changed a bit how sqlite gets build by Turley.

--- cmake/SQLite.cmake
+++ cmake/SQLite.cmake
@@ -1,14 +1,25 @@
-set(lib_name sqlite)
+message("* sqlite")
+set(SQLITE_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/../lib/sqlite")
+if (${linux})
+  set(SQLITE_LIB "${SQLITE_SOURCE_DIR}/libsqlite.a")
+else()
+  set(SQLITE_LIB "${SQLITE_SOURCE_DIR}/Release/sqlite.lib")
+endif()
 
-set(sqlite_sources 
-  ${CMAKE_CURRENT_LIST_DIR}/../lib/sqlite-amalgamation-3290000/shell.c
-  ${CMAKE_CURRENT_LIST_DIR}/../lib/sqlite-amalgamation-3290000/sqlite3.c
-  ${CMAKE_CURRENT_LIST_DIR}/../lib/sqlite-amalgamation-3290000/sqlite3.h
-  ${CMAKE_CURRENT_LIST_DIR}/../lib/sqlite-amalgamation-3290000/sqlite3ext.h
-)
-
-add_library(${lib_name} STATIC
-  ${${lib_name}_sources}
-)
-
-set_target_properties (${lib_name} PROPERTIES FOLDER 3rdParty)
+if ( NOT EXISTS "${SQLITE_LIB}" )
+  ExternalProject_Add(sqlite
+    URL "${SQLITE_SOURCE_DIR}/../sqlite.zip" 
+    SOURCE_DIR "${SQLITE_SOURCE_DIR}"
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_LIST_DIR}/SqliteList.txt" "${SQLITE_SOURCE_DIR}/CMakeLists.txt"
+    PREFIX sqlite
+    CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+    BUILD_IN_SOURCE 1
+    #no install step
+    INSTALL_COMMAND ""
+    BUILD_BYPRODUCTS ${SQLITE_LIB}
+    LOG_DOWNLOAD 1
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
+  )
+endif()
